c90528deee0e33070c14dcde93c215b66a8c74d3
Sydney Garay <38093161+sgg2123@users.noreply.github.com>
2018-06-03 21:16:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // Write your code in this file!
 function scuberGreetingForFeet(ride) {
   if (ride <= 400) {
-    return 'This one is on me!'
+    return 'This one is on me!';
   } else if (ride > 2000 && ride <= 2500) {
-    return 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.';
   } else if (ride > 2500) {
-    return 'No can do.'
+    return 'No can do.';
   }
 }
 
 function ternaryCheckCity(city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
-      return 'Thank you so much.'
+      return 'Thank you so much.';
       break;
     case 'not as generous':
-      return 'Thank you.'
+      return 'Thank you.';
       break;
     default:
-      return 'Bye.'
+      return 'Bye.';
       break;
   }
 }
